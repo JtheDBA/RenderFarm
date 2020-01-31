9d0f5dd116f3cfdf3c90753e7a2cb4f844d19688
Joel Mathias <joel.c.mathias@gmail.com>
2020-01-30 20:45:40 -0500

    finish Windows part

--- Windows/ff.ps1
+++ Windows/ff.ps1
@@ -4,16 +4,23 @@
  # @author  jmathias@cscc.edu (Joel Mathias)
  # @created 08/25/19
  # @updated 01/25/20 jmathias@cscc.edu - moved to GitHub and documented
+ # @updated 01/30/20 jmathias@cscc.edu - finished
  #
  #>
-$todoDir = "T:\MKV\_TODO\"
-$workDir = "M:\DONE\"
+$todoDir = "S:\"
+$workDir = "C:\WORK\"
 $todoFile = $todoDir + "ffmpeg.todo"
 
-Write-Host "Startinge..."
+if (!((Test-Path $todoFile -PathType Leaf) -and (Test-Path $workDir -PathType Container))) {
+    Write-Host "todo or work folders/files not found; aborted everything"
+    exit
+}
+$doneDir = "T:\"
+
+Write-Host "Starting..."
 while (((Get-Item -Path $todoFile).Length) -gt 0) { 
     $todo =	Get-Content -Path $todoFile
-#	^^^		Get-Content returns an array for more than one line but a non-array if only one line
+#	^^^^^   Get-Content returns an array for more than one line but a non-array if only one line
     if ($todo -is [array]) {
         $l = ($todo[0]).Split("`t") # split line 1 by tabs into array
         $todo[1..($todo.Length-1)] | Out-File -FilePath $todoFile -Encoding ascii # -Confirm # write todo file back out minus line 1
@@ -21,22 +28,37 @@ while (((Get-Item -Path $todoFile).Length) -gt 0) {
         $l = $todo.Split("`t")
         New-Item -Path $todoFile -Force -ItemType File > null
     }
-# set defaults
-    if ($l[1] -eq "") { $l[1] = "-map 0:v -map 0:a" } # default mapping of file 1 audio and video
-    if ($l[2] -eq "") { $l[2] = "-c:v copy" } # default to copy video stream
-    if ($l[3] -ne "") { $l[3] += ' ' } # if metadata provided, add a space for language setting
-    if ($l[4] -eq "") { $l[4] = "-c:a copy" } # default to copy audio stream ? not needed ? ffmpeg default is to copy ?
+    $l[0] = $todoDir+$l[0]
+    if ((Test-Path $l[0] -PathType Leaf)) {
 
-# augment parts of the command line
-    Write-Host (Get-Date -Format 'MMdd HHmmss') $l[0] $l[5]
-    $l[0] = ('-i "'+$todoDir+$l[0]+'" -f mp4') # create input file argument
-    $l[2] += " -movflags +faststart" # set moov atom to front of video for internet streaming to make Plex happy
-    $l[3] += "-metadata language=eng"
-    $l[5] = '"'+$workDir+$l[5]+'.mp4"' # set output file argument
+    # set defaults
+        if ($l[1] -eq "") { $l[1] = "-map 0:v -map 0:a" } # default mapping of file 1 audio and video
+        if ($l[2] -eq "") { $l[2] = "-c:v copy" } # default to copy video stream
+        if ($l[3] -ne "") { $l[3] += ' ' } # if metadata provided, add a space for language setting
+        if ($l[4] -eq "") { $l[4] = "-c:a copy" } # default to copy audio stream ? not needed ? ffmpeg default is to copy ?
 
-# run FFmpeg
-    Start-Process -FilePath 'C:\DOS\bin\ffmpeg.exe' -ArgumentList $l -Wait -WindowStyle Maximized;
-    Write-Host (Get-Date -Format 'MMdd HHmmss')
+    # augment parts of the command line
+        Write-Host (Get-Date -Format 'MMdd HHmmss') $l[0] $l[5]
+        $l[0] = ('-i "'+$l[0]+'" -f mp4') # create input file argument
+        $l[2] += " -movflags +faststart" # set moov atom to front of video for internet streaming to make Plex happy
+        $l[3] += "-metadata language=eng"
+        $l[5] = '"'+$workDir+$l[5]+'.mp4"' # set output file argument
+
+    # run FFmpeg
+        Start-Process -FilePath 'C:\DOS\bin\ffmpeg.exe' -ArgumentList $l -Wait -WindowStyle Maximized;
+        Write-Host (Get-Date -Format 'MMdd HHmmss')
+
+    # if doneDir is available, move the file there
+        if (($doneDir -ne "") -and (Test-Path $doneDir -PathType Container)) {
+            $l[5] = $l[5].Replace('"','')
+            if (Test-Path $l[5] -PathType Leaf) {
+                Move-Item -Path $l[5] -Destination $doneDir -Force 
+            }
+        }
+
+    } else {
+        Write-Host $l[0] "not found; aborted encode"
+    }
 } 
 Write-Host "Done..."
 # Shutdown-Computer
--- Windows/readme.md
+++ Windows/readme.md
@@ -3,18 +3,29 @@
 In this example the source and target machine for source videos to encode and the destination for the encoded videos. The IP for that server is `192.168.0.144` and there is a `_TODO` and `_DONE` share on that computer. The computer has
 Windows 10 Professional and I use my "Microsoft ID" to connect to the shares.
 
+
+
 ## Installation
 
-
-Directories and mount points need to be created for the source and target
-
-```
-
-
-```
+- Download [FFmpeg](https://FFmpeg.zeranoe.com/builds/) and extract it somewhere; personally I prefer to `C:\DOS` so binaries and other software executables end up in the `C:\DOS\bin` folder.
+- Put `ff.cmd` and `ff.ps1` in the same folder somewhere; `C:\Users\yourname` ??
+- Make a "work" folder somewhere
+- Edit `ff.cmd` and `ff.ps1` to fit the folders and shares in your environment.
 
 ## Starting
 
+Directories and mount points need to be created for the source and target because the command prompt version of FFmpeg does not recognize network UNC names. This must also be performed outside of PowerShell.
+The easiest way to do this and clean up afterwards is through a command prompt script `ff.cmd`
+
+```DOS bat
+NET USE S: "\\192.168.0.144\_TODO" /P:NO
+NET USE T: "\\192.168.0.144\_DONE" /P:NO
+NET USE 
+PowerShell.exe ff.ps1
+NET USE S: /DELETE
+NET USE T: /DELETE
+```
 
 ## Post Encode
 
+If you used the `$doneDir = "T:\"` variable there is nothing left to do. If you did not, you will need to get the encoded files to their final destination.
